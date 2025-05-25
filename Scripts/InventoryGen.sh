d4c4679606cbd66a677649fb9f92641f5dcad9d9
Moshibl <mohamed.gshibl@gmail.com>
2025-05-25 17:52:17 +0300

    FEAT: Added Helper Scripts

---
+++ Scripts/InventoryGen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ScriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+WorkDir="$(dirname "$ScriptDir")"
+
+InventoryFile="$WorkDir/Ansible/inventory"
+
+rm -f "$InventoryFile"
+
+{
+  echo "[Master]"
+  echo bastion
+
+  echo "[Slave]"
+  echo slave
+
+  echo "[Backend]"
+  aws ec2 describe-instances \
+    --filters "Name=tag:Role,Values=Backend" "Name=instance-state-name,Values=running" \
+    --query "Reservations[*].Instances[*].PrivateIpAddress" \
+    --output text
+} >> "$InventoryFile"
+
---
+++ Scripts/SSH-Config.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+cd ../Terraform
+terraform output -json >../Scripts/TF-Outputs.json
+MASTER_IP=$(terraform output -raw MASTER_IP)
+SLAVE_IP=$(terraform output -raw SLAVE_IP)
+KEY_PATH=$(realpath "$1")
+
+cat > ~/.ssh/config <<EOF
+Host bastion
+  HostName ${MASTER_IP}
+  User ubuntu
+  IdentityFile ${KEY_PATH}
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+
+Host slave
+  HostName ${SLAVE_IP}
+  User ubuntu
+  IdentityFile ${KEY_PATH}
+  ProxyJump bastion
+  StrictHostKeyChecking no
+  UserKnownHostsFile=/dev/null
+EOF
+
+chmod 600 ~/.ssh/config
+echo "Jump Server configured in ~/.ssh/config"
+
